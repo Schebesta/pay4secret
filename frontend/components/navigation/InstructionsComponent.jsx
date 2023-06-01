ebe4f5a2a7e40aadb4266040e500c80d167e07cc
RickOwri <mickael.rutembya@gmail.com>
2023-06-01 18:33:16 +0200

    interactAPIlevel1

--- frontend/components/navigation/InstructionsComponent.jsx
+++ frontend/components/navigation/InstructionsComponent.jsx
@@ -103,53 +103,108 @@ function requestTokens(signer, signature, setLoading, setTxData) {
 			setTxData(data);
 			setLoading(false);
 		});
-	console.log()
 }
 
 function GetTokenNumber() {
 	const { data: signer } = useSigner();
-	const [txData, setTxData] = useState(null);
+	const [txData, setTxData] = useState("");
 	const [isLoading, setLoading] = useState(false);
+	const [isLoadingAdress, setLoadingAddress] = useState(false);
+	const [address, setAddress] = useState('');
+
+	const handleInputChange = (event) => {
+		setAddress(event.target.value);
+	  };
+
+	const fetchData = () => {
+		setLoadingAddress(true);
+		setLoadingAddress(false);
+
+	// Effectuez votre fetch avec l'URL contenant l'adresse entrée par l'utilisateur
+	// Utilisez `address` dans votre URL de requête fetch
+	// Mettez à jour le state `isLoading` lorsque les données sont récupérées
+	};
+
+	// console.log(signer._address)
 
 	if (txData) return (
 		<div>
-			<p>Revenu compte est {txData}
+			<p>The account Token Number is {txData} for the address {address}
 			</p>
 		</div>
 	);
 
 	if (isLoading) return (
 		<div>
-			<p>Requesting tokens holder to be fetched...for address {}</p>
-		</div>
-	);
+	        <p>Requesting tokens holder to be fetched for address {address}</p>
+      </div>
+    );
+
 
 	return (
 		<div>
-			<p>get the token number of the address </p>
-			<button onClick={() => getTokenNumber(signer, txData, setLoading, setTxData)}>Token MTK Holded</button>
+			<div>
+				<input type="text" value={address} onChange={handleInputChange} />
+				<button onClick={fetchData}>Fetch Data</button>
+			</div>
+			<p>get the token number of the address {address}</p>
+			<button onClick={() => getTokenNumber(address, setLoading, setTxData)}>Get Token Address Holded</button>
+			{/* <button onClick={() => getTokenNumber(signer, txData, setLoading, setTxData)}>Token MTK Holded</button> */}
 		</div>
 			
 	);
 	
 }
 
-function getTokenNumber(signer, txData, setLoading, setTxData) {
+function getTokenNumber(address, setLoading, setTxData) {
+	// console.log(signer?._address)
 	setLoading(true);
 	const requestOptions = {
 		method: "GET",
 		headers: { "Content-Type": "application/json" }
-		// body: JSON.stringify({address:txData})
 	};
 
-
-
-	fetch(`http://localhost:3001/get-token-number/0x2471B1373F20f52e5ce6Cd0D08b4cE56a75acc44`, requestOptions)
-		.then(response => response)
-		.then((data) => {
-			setTxData(data);
+	// fetch(`http://localhost:3001/get-token-number/${address}`, requestOptions)
+	// .then(response1 => response1)
+	// .then((response1) => {
+	// 	setTxData1(response1);
+	// 	setLoading(false);
+	// });
+	console.log(`http://localhost:3001/get-token-number/${address}`)
+	fetch(`http://localhost:3001/get-token-number/${address}`, requestOptions)
+		.then(response => response.text())
+		.then((response) => {
+			setTxData(response);
 			setLoading(false);
 		});
-
-	console.log(requestOptions)
+	
 }
+
+
+
+// function YourComponent1() {
+// 	const [responseValue, setResponseValue] = useState('');
+// 	const [isLoading, setLoading] = useState(false);
+  
+// 	useEffect(() => {
+// 	  setLoading(true);
+  
+// 	  fetch(`http://localhost:3001/get-token-number/0x2471B1373F20f52e5ce6Cd0D08b4cE56a75acc44`, requestOptions)
+// 		.then(response => response.text())
+// 		.then((data) => {
+// 		  setResponseValue(data);
+// 		  setLoading(false);
+// 		});
+// 	}, []);
+  
+// 	if (isLoading) {
+// 	  return <p>Loading...</p>;
+// 	}
+  
+// 	return (
+// 	  <div>
+// 		<p>Response Value: {responseValue}</p>
+// 	  </div>
+// 	);
+//   }
+  
