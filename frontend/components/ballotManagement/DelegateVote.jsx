1ff99680ae91084fc1cce4ad33484414df6e3be4
RickOwri <mickael.rutembya@gmail.com>
2023-06-03 23:11:24 +0200

    assets

--- frontend/components/ballotManagement/DelegateVote.jsx
+++ frontend/components/ballotManagement/DelegateVote.jsx
@@ -4,17 +4,39 @@ import { useSigner, useNetwork, useBalance } from 'wagmi';
 import { useState, useEffect } from 'react';
 import WalletComponent from "../walletInfo/WalletInfo";
 
-function delegateTokens() {
-    const pKey = this.configService.get<string>('PRIVATE_KEY_SANGOKU');
+export function Delegate() {
+    const [txData, setTxData] = React.useState(null);
+      const [isLoading, setLoading] = React.useState(false);
+    const { data:signer} = useSigner();
+    const router = useRouter();
   
-    const wallet = new ethers.Wallet(pKey);
-
-    const signer = wallet.connect(this.provider);
-
-    const delegateTx = this.tokenContract.connect(signer).delegate(addressRECEIVER);
-
-    // this.awaitTx(delegateTx);
-
-    return delegateTx
-
-  }
+    const provider = new ethers.providers.InfuraProvider("sepolia",process.env.NEXT_PUBLIC_INFURA_KEY);
+    const tokenContract = new Contract(process.env.NEXT_PUBLIC_TOKEN_ADDRESS, tokenJson.abi, provider);
+  
+      return (
+        <div>
+          <h1>Delegate voting power</h1>
+          <button onClick={() => delegate(signer, signer._address, tokenContract, setLoading, setTxData)}>Delegate</button>
+          { 
+              isLoading? <p>Delegating voting power...</p> : <p></p>
+            }
+            {
+              txData? <p>Delegation is done {txData}</p> : <p></p>
+            }
+        </div>
+      )
+      
+    }
+  
+    function delegate(signer, address, tokenContract, setLoading, setTxData){
+     setLoading(true);
+     tokenContract.connect(signer).delegate(address)
+         .then(() => {
+           setTxData(data);
+           setLoading(false);
+           console.log("Delegation completed");
+         }).catch((err) => {
+           console.log(err);
+         });
+   }
+        
